9058b501fb68fa1c7217eed47cc1048e8436aaed
neokonstabel <konstabelneo@gmail.com>
2022-12-17 09:18:19 +0200

    Added saveTab button and functionality

--- app.js
+++ app.js
@@ -2,22 +2,42 @@ const inputBtn = document.getElementById("input-btn");
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("deleteBtn");
+const tabBtn = document.getElementById("tab-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-inputBtn.addEventListener("click", () => {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  JSON.stringify(myLeads);
-  localStorage.setItem("myLeads", myLeads);
-  renderLeads();
-  console.log(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `<li>
-    <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+    <a target='_blank' href='${leads[i]}'>${leads[i]}</a>
     </li>`;
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", () => {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
